66ad3f2e925f1a16841ec7b0411e932133f6112d
Br1zent <175925256+Br1zent@users.noreply.github.com>
2026-09-24 12:41:25 +0300

    Drop unused nuke use case change

--- lib/domain/usecases/privacy/nuke_all_data.dart
+++ lib/domain/usecases/privacy/nuke_all_data.dart
@@ -1,16 +1,13 @@
 import '../../repositories/history_repository.dart';
 import '../../repositories/bookmark_repository.dart';
-import '../../repositories/browser_data_repository.dart';
 
 class NukeAllData {
   final HistoryRepository historyRepository;
   final BookmarkRepository bookmarkRepository;
-  final BrowserDataRepository browserDataRepository;
 
   NukeAllData({
     required this.historyRepository,
     required this.bookmarkRepository,
-    required this.browserDataRepository,
   });
 
   Future<void> call({bool clearBookmarks = false}) async {
@@ -21,7 +18,5 @@ class NukeAllData {
     if (clearBookmarks) {
       await bookmarkRepository.deleteAllBookmarks();
     }
-
-    await browserDataRepository.clearAll();
   }
 }
